be5d4854a98e7f13ce8f00e36e2cf2356f7a60a1
edilson gomes de lima <edskateicevida@gmail.com>
2026-06-27 19:10:43 -0300

    atualização

---
+++ 01.py
@@ -0,0 +1,26 @@
+print("hello world ");
+
+
+if 5 > 2:
+      print("Five is greater than two!")
+
+
+print("Python is fun!")
+
+
+print("Hello World!")
+print("Have a good day.")
+print("Learning Python is fun!")
+
+print("I am", 35, "years old.")
+
+#print("Hello, World!")
+print("Cheers, Mate!")
+
+
+# Write a single-line comment
+
+# Comment out this line so it does not run:
+print("This should not run")
+
+# Add a multiline comment
